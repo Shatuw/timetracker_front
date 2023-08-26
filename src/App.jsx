d52cb71ef6f02d72d8f07032f307a71ad092131c
Shatuw <r.voigtmail@gmail.com>
2023-08-26 13:31:20 +0200

    set up dasic Routes / RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Root from './components/Root.jsx';
+import Welcome from "./components/Welcome.jsx";
+import Calender from './components/Calender.jsx';
 
 function App() {
   
-
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Root />} >
+    <Route index element={<Welcome />}/>
+    <Route path='/timetracker' element={<Calender />} />
+  </Route>
+))
   return (
     <>
-      Hello World!
-        
+      <RouterProvider router={router} />
     </>
   )
 }
