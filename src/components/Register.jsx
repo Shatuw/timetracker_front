682f2a6a27f84fcd860cbcc7295188ed59522bce
Shatuw <r.voigtmail@gmail.com>
2023-09-12 07:25:04 +0200

    nix großes

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom'
 export default function Register() {
   return (
     <>
-    <h1>Make a new Account</h1>
-    <form>
-        <label>e-mail: <input/></label>
-        <label>password: <input/></label>
-        <button>register now</button>
-    </form>
-    <Link to="/">back to login</Link>
+      <h1>Make a new Account</h1>
+      <form>
+        <label>Firstname: <input type="text" name="first_name" placeholder='Max' required/></label>
+        <label>Lastname: <input type="text" name="last_name" placeholder='Mustermann' required/></label>
+        <label>e-mail: <input type="text" name="e-mail" placeholder='max.mustermann@mustermail.com' required/></label>
+        <label>password: <input type="text" name="password" required/></label>
+        <label>password again: <input type="text" name="check_password" required/></label>
+        <button type="submit">register now</button>
+      </form>
+      <Link to="/">back to login</Link>
     </>
   )
 }
